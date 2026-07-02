96c1636eb314bcc8bd031160d6ad5bd7f39ae1a8
Brenosilvxz <josephbreno72@gmail.com>
2026-07-02 13:42:40 -0300

    FeaturedModels refeito.

--- src/components/FeaturedModels/Featuredmodels.jsx
+++ src/components/FeaturedModels/Featuredmodels.jsx
@@ -1,4 +1,5 @@
 import "./Featuredmodels.css";
+import { Link } from "react-router-dom";
 
 import bmw from "../../assets/bmw-home.jpg";
 import mclaren from "../../assets/mclaren-home.jpg";
@@ -12,6 +13,22 @@ function FeaturedModels() {
       <div className="images">
         <img src={bmw} />
         <img src={mclaren} />
+      </div>
+      <div className="Textimages">
+        <div className="Textbmw">
+          <h2>Nova BMW disponível no nosso catálogo</h2>
+          <p>Veja detalhes inéditos do nosso novo modelo!</p>
+          <Link className="Link" to="/bmw-novo-modelo">
+            Acesse aqui
+          </Link>
+        </div>
+        <div className="TextMclaren">
+          <h2>McLaren uníca do catálogo</h2>
+          <p>Homenagem a uma lenda!</p>
+          <Link className="Link" to="/Mclaren-novo-modelo">
+            Acesse aqui
+          </Link>
+        </div>
       </div>
     </section>
   );
