4dea8d10a6f0081ee05f3c4f288f3245b0603b6d
Brenosilvxz <josephbreno72@gmail.com>
2026-07-20 17:53:16 -0300

    responsividade

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,80 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import logo from "../../assets/logo.png";
 import userIcon from "../../assets/user.svg";
-import iconCart from "../../assets/cart.svg";
 import iconLocalizacao from "../../assets/localizacao.svg";
 
 function Navbar() {
-  return (
-    <div className="Navbar">
-      <img src={logo} alt="Logo MeeCars" className="Logo" />
+  const [menuAberto, setMenuAberto] = useState(false);
 
-      <ul className="Navegation">
+  return (
+    <header className="Navbar">
+
+      <div className="NavbarTop">
+
+        <img src={logo} alt="Logo MeeCars" className="Logo" />
+
+        <button
+          className="MenuButton"
+          onClick={() => setMenuAberto(!menuAberto)}
+        >
+          ☰
+        </button>
+
+      </div>
+
+      <ul className={`Navegation ${menuAberto ? "Active" : ""}`}>
+
         <li>
-          <Link className="Links" to="/">
+          <Link className="Links" to="/" onClick={() => setMenuAberto(false)}>
             Home
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/Catalogo">
+          <Link className="Links" to="/Catalogo" onClick={() => setMenuAberto(false)}>
             Modelos
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/destaques">
+          <Link className="Links" to="/destaques" onClick={() => setMenuAberto(false)}>
             Destaques
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/sobre">
+          <Link className="Links" to="/sobre" onClick={() => setMenuAberto(false)}>
             Sobre
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/favoritos">
+          <Link className="Links" to="/favoritos" onClick={() => setMenuAberto(false)}>
             ❤️ Favoritos
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/login">
+          <Link className="Links" to="/login" onClick={() => setMenuAberto(false)}>
             <img src={userIcon} alt="Perfil" className="UserIcon" />
           </Link>
         </li>
 
         <li>
-          <Link className="Links" to="/localize">
+          <Link className="Links" to="/localize" onClick={() => setMenuAberto(false)}>
             <div className="Sourch">
               <img src={iconLocalizacao} alt="Localizar" className="Local" />
               <h3>Localize perto de você</h3>
             </div>
           </Link>
         </li>
+
       </ul>
-    </div>
+
+    </header>
   );
 }
 
